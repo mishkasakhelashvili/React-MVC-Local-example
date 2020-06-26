0aa3f442329de50959997e9d2ca18bd7b1edfa93
mishkasakhelashvili <56650453+mishkasakhelashvili@users.noreply.github.com>
2020-06-26 12:43:22 +0400

    react hook example

---
+++ src/components/BookList.js
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+
+import { BookContext } from '../contexts/BookContext';
+import BookDetails from './BookDetails';
+
+const BookList = () => {
+    const { books } = useContext(BookContext);
+    return books.length ? (
+        <div className='book-list'>
+            <ul>
+                { books.map( book => {
+                    return ( 
+                        <BookDetails 
+                            key={book.id} 
+                            book={book} 
+                        /> 
+                    );
+                })}
+            </ul>
+        </div>
+    ) : (
+        <div className='empty'> No Items </div>
+    )
+};
+
+export default BookList;
